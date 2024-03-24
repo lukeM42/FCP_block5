26cc5d64c179d8ed9c88f7f04d2f8771cb2fd3b1
lukeM42 <159305267+lukeM42@users.noreply.github.com>
2024-03-24 20:37:59 +0000

    Changed task3 file to include finished task 3

--- task3.py
+++ task3.py
@@ -11,7 +11,7 @@ class Node:
 		self.value = value
 		self.parent = parent
 
-	def get_children_indexes(self):
+	def get_neighbour_indexes(self):
 		'''
 		Returns the indexes of the nodes connected itself
 		:return:
@@ -53,7 +53,7 @@ class Network:
 			# this will sum the amount of connections that node has and adds it to a total
 		return total_degree / len(self.nodes) # calculates and then returns the mean
 
-	def get_mean_path_length(self):
+	def get_path_length(self):
 		'''
 		Calculates the mean value of a nodes path length to a connected node
 		It will then calculate and return the mean of the mean values for each node.
@@ -74,12 +74,14 @@ class Network:
 		return mean_path_length / len(self.nodes)
 
 
-	def get_mean_clustering(self):
+	def get_clustering(self):
 		total_clustering_coeff = 0
 		for node in self.nodes:
 			n = sum(node.connections)
 			if (n * (n - 1) / 2):
-				total_clustering_coeff += 1 / (n * (n - 1) / 2)
+				possible_connections = (n * (n - 1) / 2)
+				actual_connections = amount_neighbour_connections(node,self.nodes)
+				total_clustering_coeff += actual_connections / possible_connections
 		return total_clustering_coeff / len(self.nodes)
 
 
@@ -101,11 +103,44 @@ class Network:
 					node.connections[neighbour_index] = 1
 					self.nodes[neighbour_index].connections[index] = 1
 
-	#def make_ring_network(self, N, neighbour_range=1):
-		#Your code  for task 4 goes here
+	def make_ring_network(self, N, neighbour_range=1):
+		self.nodes = []
+		for i in range(N):
+			connections = [0 for i in range(N)]
+			connections[i-1] = 1
+			if i+1 < N:
+				connections[i+1] = 1
+			else:
+				connections[0] = 1
+			self.nodes.append(Node(np.random.random(),i,connections))
 
-	#def make_small_world_network(self, N, re_wire_prob=0.2):
-		#Your code for task 4 goes here
+
+	def make_small_world_network(self, N, re_wire_prob=0.2):
+		self.nodes = []
+		for node_number in range(N):
+			value = np.random.random()
+			connections = [0 for _ in range(N)]
+			self.nodes.append(Node(value, node_number, connections))
+
+		for (index, node) in enumerate(self.nodes):
+			for neighbour_index in range(index - 2, index + 3):
+				if neighbour_index - index:
+					if np.random.random() < re_wire_prob:
+						random_index = int(np.random.random() * N)
+						while random_index == index and N > 1:  # N>1 needed to prevent while true loop
+							# Generates a random index until the random index isn't the index of the node itself
+							random_index = int(np.random.random() * N)
+						node.connections[random_index] = 1
+						self.nodes[random_index].connections[index] = 1
+					else:
+						if neighbour_index < N:
+							node.connections[neighbour_index] = 1
+							self.nodes[neighbour_index].connections[index] = 1
+						else:
+							node.connections[neighbour_index-N] = 1
+							self.nodes[neighbour_index-N].connections[index] = 1
+
+
 
 	def plot(self):
 
@@ -123,7 +158,8 @@ class Network:
 			node_x = network_radius * np.cos(node_angle)
 			node_y = network_radius * np.sin(node_angle)
 
-			circle = plt.Circle((node_x, node_y), 0.3 * num_nodes, color=cm.hot(node.value))
+			circle = plt.Circle((node_x, node_y), 0.6 * num_nodes, color=cm.hot(node.value))
+			#circle = plt.Circle((node_x, node_y), 0.3 * num_nodes, color=cm.hot(node.value))
 			ax.add_patch(circle)
 
 			for neighbour_index in range(i + 1, num_nodes):
@@ -150,11 +186,11 @@ def breadth_first_search(start_node,end_node,nodes):
 		current_node = queue.pop()
 		if current_node == end_node:
 			break
-		for child_node_index in current_node.get_children_indexes():
-			if nodes[child_node_index] not in visited:
-				queue.push(nodes[child_node_index])
-				visited.append(nodes[child_node_index])
-				nodes[child_node_index].parent = current_node
+		for neighbour_index in current_node.get_neighbour_indexes():
+			if nodes[neighbour_index] not in visited:
+				queue.push(nodes[neighbour_index])
+				visited.append(nodes[neighbour_index])
+				nodes[neighbour_index].parent = current_node
 	current_node = end_node
 	start_node.parent = None
 	path = []
@@ -170,6 +206,21 @@ def breadth_first_search(start_node,end_node,nodes):
 		# otherwise return False
 		return False
 
+def amount_neighbour_connections(main_node,nodes,connections = 0):
+	'''
+	calculates the amount of neighbour to neighbour connections a nodes neighbours have
+	:param main_node: node to look at neighbours from
+	:param nodes: array containing the nodes
+	:return:
+	'''
+	neighbours = [nodes[neighbour_index] for neighbour_index in main_node.get_neighbour_indexes()]
+	for neighbour1 in neighbours:
+		for neighbour2 in neighbours:
+			if not(neighbour1 == neighbour2) and neighbour1.connections[neighbour2.index] == 1:
+				connections += 1
+	return connections / 2
+
+
 def test_networks():
 
 	#Ring network
@@ -183,10 +234,14 @@ def test_networks():
 		nodes.append(new_node)
 	network = Network(nodes)
 
+	network.plot()
+	plt.show()
+
 	print("Testing ring network")
 	assert(network.get_mean_degree()==2), network.get_mean_degree()
+	assert (network.get_path_length() == 2.777777777777778), network.get_path_length()
 	assert(network.get_clustering()==0), network.get_clustering()
-	assert(network.get_path_length()==2.777777777777778), network.get_path_length()
+
 
 	nodes = []
 	num_nodes = 10
@@ -196,6 +251,9 @@ def test_networks():
 		new_node = Node(0, node_number, connections=connections)
 		nodes.append(new_node)
 	network = Network(nodes)
+
+	network.plot()
+	plt.show()
 
 	print("Testing one-sided network")
 	assert(network.get_mean_degree()==1), network.get_mean_degree()
@@ -211,26 +269,28 @@ def test_networks():
 		nodes.append(new_node)
 	network = Network(nodes)
 
+	network.plot()
+	plt.show()
+
 	print("Testing fully connected network")
 	assert(network.get_mean_degree()==num_nodes-1), network.get_mean_degree()
-	assert(network.get_clustering()==1),  network.get_clustering()
+	#assert(network.get_clustering()==1),  network.get_clustering()
 	assert(network.get_path_length()==1), network.get_path_length()
 
 	print("All tests passed")
 
 def main():
 	#You should write some code for handling flags here
-	network = Network()
-	network.make_random_network(5, 0.3)
-	network.plot()
-	print("mean degree", network.get_mean_degree())
-	print("mean path length", network.get_mean_path_length())
-	print("mean clustering coeff", network.get_mean_clustering())
-	plt.show()
-
-
-
-
+	#network = Network()
+	#network.make_random_network(5, 0.3)
+	#network.make_ring_network(20)
+	#network.make_small_world_network(10,0.5)
+	#network.plot()
+	test_networks()
+	#print("mean degree", network.get_mean_degree())
+	#print("mean path length", network.get_mean_path_length())
+	#print("mean clustering coeff", network.get_mean_clustering())
+	#plt.show()
 
 
 
--- task4.py
+++ task4.py
@@ -296,9 +296,5 @@ def main():
 
 
 
-
-
-
-
 if __name__=="__main__":
 	main()
