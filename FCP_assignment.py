92b2b7c98c961c3439711464e54e31fa6d1b0b6d
lukeM42 <159305267+lukeM42@users.noreply.github.com>
2024-04-30 21:42:04 +0100

    uploaded task 1 and sorted flags

--- FCP_assignment.py
+++ FCP_assignment.py
@@ -311,38 +311,50 @@ This section contains code for the Ising Model - task 1 in the assignment
 ==============================================================================================================
 '''
 
-def calculate_agreement(population, row, col, external=0.0):
-	'''
-	This function should return the *change* in agreement that would result if the cell at (row, col) was to flip it's value
-	Inputs: population (numpy array)
-			row (int)
-			col (int)
-			external (float)
-	Returns:
-			change_in_agreement (float)
-	'''
+def calculate_agreement(population, row, col, external=0.0, alpha=1.0):
+    '''
+    This function returns the extent to which a cell agrees with its neighbors.
+    Inputs: population (numpy array)
+            row (int)
+            col (int)
+            external (float)
+    Returns:
+            change_in_agreement (float)
+    '''
+    num_rows, num_cols = population.shape
 
-	#Your code for task 1 goes here
+    # Calculate current agreement
+    lower_neighbour_agreement = population[(row + 1) % num_rows, col] * population[row, col]
+    upper_neighbour_agreement = population[(row - 1) % num_rows, col] * population[row, col]
+    left_neighbour_agreement = population[row, (col - 1) % num_cols] * population[row, col]
+    right_neighbour_agreement = population[row, (col + 1) % num_cols] * population[row, col]
 
-	return np.random * population
+    sum_of_agreements = upper_neighbour_agreement + lower_neighbour_agreement + \
+                        left_neighbour_agreement + right_neighbour_agreement
+    agreement = sum_of_agreements + (external * population[row, col])
 
-def ising_step(population, external=0.0):
-	'''
-	This function will perform a single update of the Ising model
-	Inputs: population (numpy array)
-			external (float) - optional - the magnitude of any external "pull" on opinion
-	'''
-	
-	n_rows, n_cols = population.shape
-	row = np.random.randint(0, n_rows)
-	col = np.random.randint(0, n_cols)
+    return agreement
 
-	agreement = calculate_agreement(population, row, col, external=0.0)
+def ising_step(population, alpha, external):
+    '''
+    This function will perform a single update of the Ising model
+    Inputs: population (numpy array)
+          external (float) - optional - the magnitude of any external "pull" on opinion
+    '''
 
-	if agreement < 0:
-		population[row, col] *= -1
+    n_rows, n_cols = population.shape
+    row = np.random.randint(0, n_rows)
+    col = np.random.randint(0, n_cols)
+    agreement = calculate_agreement(population, row, col)
+    p = np.exp(-(agreement) / alpha)
+    print(p, "p")
 
-	#Your code for task 1 goes here
+    critical_value = np.random.random()
+    if critical_value < p or agreement < 0:
+        population[row, col] *= -1
+
+    return population
+
 
 def plot_ising(im, population):
 	'''
@@ -387,21 +399,20 @@ def test_ising():
 	print("Tests passed")
 
 
-def ising_main(population, alpha=None, external=0.0):
+def ising_main(population, alpha, external):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.set_axis_off()
 
-	fig = plt.figure()
-	ax = fig.add_subplot(111)
-	ax.set_axis_off()
-	im = ax.imshow(population, interpolation='none', cmap='RdPu_r')
+    # Iterating an update 100 times
+    for frame in range(100):
+        # Iterating single steps 1000 times to form an update
+        for step in range(1000):
+            ising_step(population, alpha, external)
+        print('Step:', frame, end='\r')
+        im = ax.imshow(population, interpolation='none', cmap='RdPu_r')
 
-	# Iterating an update 100 times
-	for frame in range(100):
-		# Iterating single steps 1000 times to form an update
-		for step in range(1000):
-			ising_step(population, external)
-		print('Step:', frame, end='\r')
-		plot_ising(im, population)
-
+        plot_ising(im, population)
 
 '''
 ==============================================================================================================
@@ -556,7 +567,7 @@ def main():
 	# Ising model flags
 	parser.add_argument("-ising_model", action='store_true')
 	parser.add_argument("-external", nargs=1, default=0, type=int)
-	parser.add_argument("-alpha", nargs=1, default=0, type=int)
+	parser.add_argument("-alpha", nargs=1, default=1, type=int)
 	parser.add_argument("-test_ising", action='store_true')
 
 	# Defuant model flags
@@ -581,6 +592,16 @@ def main():
 	args = parser.parse_args()
 
 	# Ising model flag handling
+	if args.ising_model:
+		if type(args.alpha) == list:
+			args.alpha = args.alpha[0]
+		if type(args.external) == list:
+			args.external = args.external[0]
+		population = np.random.choice([-1, 1], size=(100, 100))
+		ising_main(population, args.alpha, args.external)
+
+	if args.test_ising:
+		test_ising()
 
 	# Defuant model flag handling
 	if args.test_defuant:
